b9972500173f81513aa4db5e183eddb096f3aba9
Obed <jo.cabanzo@uniandes.edu.co>
2022-04-20 23:42:29 -0500

    Avance1

--- src/data.js
+++ src/data.js
@@ -1,4 +1,5 @@
 "use strict";
+//Crea y llena una lista de series con los datos dados
 let series = [];
 let serie1 = new Serie(1, "Breaking Bad", "AMC", 5, "Set and filmed in Albuquerque, New Mexico, the series tells the story of Walter White, a struggling and depressed high school chemistry teacher who is diagnosed with lung cancer", "https://www.amc.com/shows/breaking-bad", "https://i.imgur.com/GGje0vc.jpg");
 let serie2 = new Serie(2, "Orange Is the New Black", "Netflix", 6, "The series begins revolving around Piper Chapman, a woman in her thirties living in New York City who is sentenced to 15 months in Litchfield Penitentiary", "https://www.netflix.com/co/title/70242311", "https://i.imgur.com/EvKe48G.jpg");
@@ -13,8 +14,9 @@ series.push(serie4);
 series.push(serie5);
 series.push(serie6);
 let i = 0;
-var rowsNode1 = document.getElementById("rowsTable");
+var rowsNode = document.getElementById("rowsTable");
 let seasonsNumber = 0;
+// Para cada serie en la lista, extrae sus elementos y los inserta como una nueva fila de la tabla
 while (i < series.length) {
     let serie = series[i];
     let attributes = serie.darSerie();
@@ -25,9 +27,10 @@ while (i < series.length) {
     seasonsNumber += seasons;
     let row = '<th scope="row"> ' + id + '</th>' + '<td> ' + name + ' </td>'
         + '<td> ' + channel + ' </td>' + '<td> ' + seasons + ' </td>';
-    rowsNode1.insertAdjacentHTML('afterend', row);
+    rowsNode.insertAdjacentHTML('afterend', row);
     i++;
 }
+//Despliega la fila que muestra el numero promedio de temporadas
 var rowsNode1 = document.getElementById("row2");
 let seasonsAverage = seasonsNumber / series.length;
 rowsNode1.insertAdjacentHTML('afterend', '<p>El promedio de temporadas es: ' + seasonsAverage + ' </p>');
